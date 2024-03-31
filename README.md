482a15be5a8ecc20521810ecb2b1b1492ecaa1ce
zombie <onetouchX9@gmail.com>
2024-04-01 01:38:42 +0600

    fetching sample data from mongodb

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 План:
 1)Поднять веб сервер на localhost:5656
-2)Установить соединение с MongoDB
-3)Разработать первый роут get_token:
+2)Установить зависимости jwt и mongodb driver:
+    go get go.mongodb.org/mongo-driver/mongo
+    go get github.com/dgrijalva/jwt-go
+    go get github.com/joho/godotenv
+3)Установить соединение с MongoDB
+4)Разработать первый роут get_token:
     
 
 
-4)Разработать второй роут refresh_token:
+
+5)Разработать второй роут refresh_token:
 
 
+mongo_username:overcoder9
+mongo_password:gm244922ssFqaLRR
+mongo_url: mongodb+srv://overcoder9:gm244922ssFqaLRR@default.pkdymz1.mongodb.net/?retryWrites=true&w=majority&appName=Default
--- main.go
+++ main.go
@@ -1,19 +1,68 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	//mongo
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	http.HandleFunc("/", greeter)
+	go start_server()
+	connect_mongo()
+	select {}
+}
+
+func start_server() {
+	http.HandleFunc("/get_token", get_token)
+	http.HandleFunc("/refresh_token", refresh_token)
 	err := http.ListenAndServe(":5656", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func greeter(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello Yopta")
+func connect_mongo() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb+srv://overcoder9:gm244922ssFqaLRR@default.pkdymz1.mongodb.net/?retryWrites=true&w=majority&appName=Default"))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	coll := client.Database("sample_mflix").Collection("movies")
+	title := "Back to the Future"
+
+	var result bson.M
+	err = coll.FindOne(context.TODO(), bson.D{{"title", title}}).Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		fmt.Printf("No document was found with the title %s\n", title)
+		return
+	}
+	if err != nil {
+		panic(err)
+	}
+	jsonData, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", jsonData)
+}
+
+func get_token(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Привет!")
+}
+
+func refresh_token(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Привет refresh")
 }
